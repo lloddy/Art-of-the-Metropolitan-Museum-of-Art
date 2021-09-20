fd5852415f43cf5dd87a106ebd711d08f8e87d56
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-09-20 17:40:36 -0400

    WIP: script workign

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(function () {
+
+    const BASE_URL = 'https://www.metmuseum.org/api/collection/collectionlisting'
+    let artData;
+
+    // chache elements
+    const $form = $('form');
+    const $input = $('input[type="text"]');
+    const $main = $('main');
+
+    // event listeners
+    $form.on('submit', handleGetArt)
+
+    //functions
+    function handleGetArt(event) {
+
+        event.preventDefault();
+
+        const artistName = $input.val();
+
+        $input.val("");
+
+        $.ajax(`${BASE_URL}?q=${artistName}&offset=0&pageSize=0&sortBy=Relevance&sortOrder=asc&perPage=20&`).then(function (data) {
+            artData = data;
+            render();
+
+        }, function (error) {
+            console.log(error);
+        })
+    }
+
+    function render() {
+
+        const htmlCards = artData.results.map(function (artInfo) {
+
+            return `<article>
+                        <p><img src="${artInfo.image}"</p>
+                        <h2>Artist: ${artInfo.artist}</h2>
+                        <p>Title: ${artInfo.title}</p>
+                        <p>Medium: ${artInfo.medium}</p>
+                        <p>${artInfo.teaserText}</p>       
+                    </article>`;
+
+        });
+        console.log(artData)
+
+        $main.html(htmlCards);
+    }
+
+
+
+})
